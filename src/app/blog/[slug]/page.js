a15bb762a2d8cf5650738be06edf01aed8414ce6
Lviathannn <valorant.leviathan@gmail.com>
2023-09-18 17:55:12 +0700

    a

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,19 +1,19 @@
 import BlogDetailSection from "@/section/blog/BlogDetailSection";
-import { getData } from "@/utils/getData";
+// import { getData } from "@/utils/getData";
 
-export async function generateMetadata({ params }) {
-    const data = await getData(
-        `https://cdn.contentful.com/spaces/${process.env.SPACE_id}/entries?access_token=${process.env.ACCESS_TOKEN}&content_type=post&fields.slug=${params.slug}`
-    );
+// export async function generateMetadata({ params }) {
+//     const data = await getData(
+//         `https://cdn.contentful.com/spaces/${process.env.SPACE_id}/entries?access_token=${process.env.ACCESS_TOKEN}&content_type=post&fields.slug=${params.slug}`
+//     );
 
-    return {
-        title: data.items[0].fields.title,
-        description: data.items[0].fields.title,
-        openGraph: {
-            images: ["https:" + data.includes.Asset[0].fields.file.url],
-        },
-    };
-}
+//     return {
+//         title: data.items[0].fields.title,
+//         description: data.items[0].fields.title,
+//         openGraph: {
+//             images: ["https:" + data.includes.Asset[0].fields.file.url],
+//         },
+//     };
+// }
 
 export default function Slug({ params }) {
     return (
